6713c572d0eb6a6611174f747b65c376352c4eab
João <71902390+Lichston@users.noreply.github.com>
2023-08-30 17:48:43 -0300

    Add files via upload

---
+++ biblioteca/src/test/java/com/bcopstein/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcopstein.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
